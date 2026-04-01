3e349cb466176d25ba8226e3c797ee87ca631508
marcus.santos <marcus.santos@bycoders.com.br>
2026-04-01 16:53:01 -0300

    environments

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3786',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const frontendHost = env.VITE_HOST
+  const frontendPort = Number(env.VITE_PORT)
+  const apiHost = env.VITE_API_HOST
+  const apiPort = Number(env.VITE_API_PORT)
+
+  return {
+    plugins: [vue()],
+    server: {
+      host: frontendHost,
+      port: frontendPort,
+      proxy: {
+        '/api': {
+          target: `http://${apiHost}:${apiPort}`,
+          changeOrigin: true
+        }
       }
     }
   }
