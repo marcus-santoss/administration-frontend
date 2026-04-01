c0ca6c39f2d26b5d30fd9e9bc537321d3ca8e5bc
marcus.santos <marcus.santos@bycoders.com.br>
2026-04-01 18:16:12 -0300

    Adição de valores default para envvars

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
-  const frontendHost = env.VITE_HOST
-  const frontendPort = Number(env.VITE_PORT)
-  const apiHost = env.VITE_API_HOST
-  const apiPort = Number(env.VITE_API_PORT)
+  const frontendHost = env.VITE_HOST || 'localhost'
+  const frontendPort = Number(env.VITE_PORT || 5173)
+  const apiHost = env.VITE_API_HOST || 'localhost'
+  const apiPort = Number(env.VITE_API_PORT || 3786)
 
   return {
     plugins: [vue()],
